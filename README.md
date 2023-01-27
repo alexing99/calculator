0aa76d989dc897cd76bf688d1bffc5e9f7c020a4
Alex Inglis <alexinglis7799@gmail.com>
2023-01-27 15:05:26 +0800

    check if buttons are numbers first and also concatenate numbers on display

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+
+This is the final project of ODIN FOUNDATIONS and it will practice all the skills learned from HTML, CSS, Javascript, and DOM manipulation to make a basic calculator with four operators. 
--- script.js
+++ script.js
@@ -18,13 +18,16 @@ function operate (operator, a, b) {
 
 }
 
-displayValue = 0;
+displayValue = "";
 
 const numButt = document.querySelectorAll("button") 
     numButt.forEach((button) => {
         button.addEventListener ('click', ()=> {
-        displayValue = button.id;
-        console.log(button.id)
+        if (isNaN(Number(button.id))) {
+            
+        } else {
+            displayValue += button.id;
+        }
 
     const display = document.querySelector(".display") 
     display.textContent = displayValue;    
