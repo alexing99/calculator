a38b51175c937526c8bff823c1cf481106afb2e7
Alex Inglis <alexinglis7799@gmail.com>
2023-01-27 14:02:21 +0800

    add event listeners to the buttons which update display

--- script.js
+++ script.js
@@ -15,5 +15,21 @@ function divide (a,b) {
 }
 
 function operate (operator, a, b) {
-    
+
 }
+
+displayValue = 0;
+
+const numButt = document.querySelectorAll("button") 
+    numButt.forEach((button) => {
+        button.addEventListener ('click', ()=> {
+        displayValue = button.id;
+        console.log(button.id)
+
+    const display = document.querySelector(".display") 
+    display.textContent = displayValue;    
+    });
+});
+
+
+
