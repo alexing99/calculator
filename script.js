6fc430071e4f623feae442141c9eb1703f1bc42d
Alex Inglis <alexinglis7799@gmail.com>
2023-01-28 01:11:41 +0800

    make delete and clear buttons functionality

--- script.js
+++ script.js
@@ -15,24 +15,56 @@ function divide (a,b) {
 }
 
 function operate (operator, a, b) {
-
+    if (operator === "/") {
+    displayValue = divide (a, b);
+    }
 }
 
-displayValue = "";
+let displayValue = "";
+let operator = "";
+let a = "";
+let b = "";
+
+function displayNow(content) {
+    const display = document.querySelector(".display") 
+    display.textContent = content;  
+};
 
 const numButt = document.querySelectorAll("button") 
     numButt.forEach((button) => {
         button.addEventListener ('click', ()=> {
         if (isNaN(Number(button.id))) {
-            
         } else {
-            displayValue += button.id;
         }
-
-    const display = document.querySelector(".display") 
-    display.textContent = displayValue;    
+    displayNow(displayValue);  
     });
 });
 
+/*const opButt = document.querySelectorAll("button")
+    opButt.forEach((button) => {
+        button.addEventListener ('click', ()=> {
+        if (isNaN(Number(button.id))) {
+            operator = button.id;
+        } else {
+    });
+});
+*/
 
+const clear = document.querySelector("#clear")
+    clear.addEventListener ('click', () => {
+       displayValue = "";
+       displayNow("");  
+    });
+
+const backspace = document.querySelector("#delete")
+    backspace.addEventListener ('click', () => {
+        displayValue = displayValue.slice(0, -1);
+    displayNow(displayValue);
+    });
+
+const equals = document.querySelector("#equals")
+    equals.addEventListener ('click', () => {
+        operate (operator, a, b);
+        console.log ();
+    });
 
