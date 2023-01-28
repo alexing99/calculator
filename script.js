9e65f4b9bf7049ebb05f9dd70c453b0a67f9f36f
Alex Inglis <alexinglis7799@gmail.com>
2023-01-28 13:59:11 +0800

    redistribute addeventlisteners for buttons and add functionality to all calculations

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
+/*
+To Do List
+
+- allow b to be negative
+- only one decimal point 
+- round 
+- fix floating point 
+- link to keyboard
+- style
+
+*/
+
+
+
 function add (a,b){
-    return a + b;
-}
+    return Number(a) + Number(b);
+};
 
 function subtract (a,b){
     return a - b;
-}
+};
 
 function multiply (a,b) {
     return a * b;
-}
+};
 
 function divide (a,b) {
     return a / b;
-}
+};
 
 function operate (operator, a, b) {
-    if (operator === "/") {
-    displayValue = divide (a, b);
+    if (operator === "/" && b === "0") {
+        displayValue = "NO WAY BUDDY!"
+        displayNow (displayValue)
+        b = '';
+    } else 
+    switch (true) {
+        case operator === "/":
+            displayValue = divide (a, b);
+            displayNow (displayValue);
+            break;
+
+        case operator === "x":
+            displayValue = multiply (a, b);
+            displayNow (displayValue);
+            break;
+           
+        case operator === "-":
+            displayValue = subtract (a, b);
+            displayNow (displayValue);
+            break;
+            
+        case operator === "+":
+            displayValue = add (a, b);
+            displayNow (displayValue);
+            break;
     }
-}
+};
 
 let displayValue = "";
 let operator = "";
@@ -30,41 +67,58 @@ function displayNow(content) {
     display.textContent = content;  
 };
 
-const numButt = document.querySelectorAll("button") 
+const numButt = document.querySelectorAll(".number-buttons button")
     numButt.forEach((button) => {
-        button.addEventListener ('click', ()=> {
-        if (isNaN(Number(button.id))) {
-        } else {
-        }
-    displayNow(displayValue);  
+    button.addEventListener ('click', ()=> {    
+        if (a != ""){
+            b += button.id;
+            displayValue = b;
+            displayNow(displayValue);
+            console.log ("b=",b)
+        } else displayValue += button.id;
+        displayNow (displayValue);
     });
 });
 
-/*const opButt = document.querySelectorAll("button")
+const opButt = document.querySelectorAll(".operator-buttons button")
     opButt.forEach((button) => {
-        button.addEventListener ('click', ()=> {
-        if (isNaN(Number(button.id))) {
+    button.addEventListener ('click', ()=> {    
+        if (operator != "") {
+            operate (operator, a, b);
+        }
+        if (displayValue === "" && button.id === "-") {
+            displayValue = button.id;
+            displayNow (displayValue);
+        } else if (displayValue != "") {
+            b = "";
+            a = displayValue;
             operator = button.id;
-        } else {
+            console.log ("a=", a)
+            console.log ("op=", operator)
+        }
     });
 });
-*/
 
 const clear = document.querySelector("#clear")
     clear.addEventListener ('click', () => {
-       displayValue = "";
-       displayNow("");  
+        displayValue = "";
+        displayNow("");  
+        a = "";
+        b = "";
+        operator = "";
     });
 
 const backspace = document.querySelector("#delete")
     backspace.addEventListener ('click', () => {
         displayValue = displayValue.slice(0, -1);
-    displayNow(displayValue);
+        if (b != '') {
+            b = displayValue;
+        }
+        displayNow(displayValue);
     });
 
 const equals = document.querySelector("#equals")
     equals.addEventListener ('click', () => {
         operate (operator, a, b);
-        console.log ();
     });
 
